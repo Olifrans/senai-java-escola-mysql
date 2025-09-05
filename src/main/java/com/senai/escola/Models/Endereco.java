765383c674ff10550984a037b6e545b48b20583e
Francisco Oliveira <fransoliveira@gmail.com>
2025-09-04 21:27:03 -0300

    java

---
+++ src/main/java/com/senai/escola/Models/Endereco.java
@@ -0,0 +1,30 @@
+package com.senai.escola.Models;
+
+public class Edereco {
+    private Long id;
+    private  String cep;
+    
+}
+
+
+
+
+
+
+//URL: viacep.com.br/ws/01001000/json/
+//
+//        {
+//        "cep": "01001-000",
+//        "logradouro": "Praça da Sé",
+//        "complemento": "lado ímpar",
+//        "unidade": "",
+//        "bairro": "Sé",
+//        "localidade": "São Paulo",
+//        "uf": "SP",
+//        "estado": "São Paulo",
+//        "regiao": "Sudeste",
+//        "ibge": "3550308",
+//        "gia": "1004",
+//        "ddd": "11",
+//        "siafi": "7107"
+//        }
